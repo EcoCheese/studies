34f172b84de4d0e6f1a6b5ebd9cc2b4955740a7b
Illya <41628011+EcoCheese@users.noreply.github.com>
2019-05-02 16:13:35 +0300

    Update main.c

--- lab_1/lab_1/main.c
+++ lab_1/lab_1/main.c
@@ -3,10 +3,10 @@
 
 int main (){
     
-    float a = 0.5 , b = 3.1 , c = 0.4 , x , y;
+    float a = 0.5 , b = 3.1 , c = 0.4 , x , y; // a, b and c are given in task
     
-    x = sqrt(asin(2*c) + exp(-2*c)*(c+b));
-    y = pow(cos(pow(c, 3)), 2) - c / sqrt(pow(a,2)+pow(b, 2));
+    x = sqrt(asin(2*c) + exp(-2*c)*(c+b));  //equasion for x
+    y = pow(cos(pow(c, 3)), 2) - c / sqrt(pow(a,2)+pow(b, 2)); // equasion for y
     
     printf("x = %f\n", x);
     printf("y = %f\n", y);
